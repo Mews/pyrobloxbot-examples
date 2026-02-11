fcbee8e13cee9551bca8f41e269c04eef818666a
Mews <60406199+Mews@users.noreply.github.com>
2026-02-11 23:57:37 +0000

    Add macros for surf script

---
+++ surf-macros/main.py
@@ -0,0 +1,11 @@
+import pyrobloxbot as bot
+import keyboard
+
+bot.options.force_focus = False
+
+keyboard.add_hotkey("u", lambda:bot.chat("/spec"))
+keyboard.add_hotkey("i", lambda:bot.chat("/serverlist"))
+keyboard.add_hotkey("o", lambda:bot.chat("/style"))
+
+while True:
+    bot.wait(0.1)
