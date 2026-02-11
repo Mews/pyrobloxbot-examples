0c1df5ea2d58ac2914fa5ab75472d07e60534065
Mews <60406199+Mews@users.noreply.github.com>
2026-02-11 17:08:13 +0000

    Add playtime rewards bot for Tank Game!

---
+++ tank-game-playtime-rewards/main.py
@@ -0,0 +1,36 @@
+import pyrobloxbot as bot
+import time
+
+def is_in_server():
+    return bot.image_is_visible("index_button.png")
+
+
+def join_server_and_wait():
+    tank_game_id = 119789365111500
+    bot.join_game(tank_game_id)
+
+    while is_in_server():
+        pass
+
+    while not is_in_server():
+        pass
+
+
+def move_for_4_minutes():
+    start_time = time.perf_counter()
+    while (time.perf_counter() - start_time) < 4*60:
+        bot.walk_left(1)
+        bot.walk_right(1)
+
+
+def open_tab_and_collect_rewards():
+    bot.ui_navigate("down", "click", "left", "left", "click", "left", "click", "left", "click")
+
+total_diamonds = 0
+while True:
+    join_server_and_wait()
+    move_for_4_minutes()
+    open_tab_and_collect_rewards()
+
+    total_diamonds += 1250
+    print("Diamonds farmed:", total_diamonds)
